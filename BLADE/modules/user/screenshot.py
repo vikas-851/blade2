ea8d3fc17b9ef4f466a501fd65fda5d2182060f1
ishu9805 <abhishek47903@gmail.com>
2024-08-24 22:06:46 +0530

    Update screenshot.py

--- BLADE/modules/user/screenshot.py
+++ BLADE/modules/user/screenshot.py
@@ -17,9 +17,9 @@ async def screenshot(bot: Client, message: Message):
         message.delete(),
         bot.invoke(
             functions.messages.SendScreenshotNotification(
-                peer=await X.resolve_peer(message.chat.id),
+                peer=await BLADE.resolve_peer(message.chat.id),
                 reply_to_msg_id=0,
-                random_id=X.rnd_id(),
+                random_id=BLADE.rnd_id(),
             )
         ),
     )

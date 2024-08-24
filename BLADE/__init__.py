197c1bbf3a9b71f4dd368b75b41a25b1b9788c09
ishu9805 <abhishek47903@gmail.com>
2024-08-24 21:17:45 +0530

    Update __init__.py

--- BLADE/__init__.py
+++ BLADE/__init__.py
@@ -7,7 +7,7 @@ from logging.handlers import RotatingFileHandler
 from typing import Any, Dict
 
 
-from aiohttp import ClientSession
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from gpytranslate import Translator
 from pyrogram import Client
@@ -94,7 +94,7 @@ LOOP = asyncio.get_event_loop()
 
 trl = Translator()
 
-aiosession = ClientSession()
+
 
 CMD_HELP = {}
 
